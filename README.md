94b2856f402ab0545de690d14e2dafedadb61513
Chris Clynes <chrisclynes@live.com>
2022-04-05 23:24:08 -0400

    first commit, working endpoints, not hosted

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dependencies 
+
+node modules
+request
+request-promise
+express
+nodejs
+nodemon
+
+
+# NOTE 
+This api has not been hosted
+//this api can be hosted on Heroku and endpoints can be created with rapid Api 
+//add node modules to gitignore list to not push them to Heroku when uploading
+
+local call to test endpoint product information:
+localhost:5000/products/:productId
+
+productId can be had from any amazon product in URL example: /dp/B08NYL4VPJ/
+
+the search feature will provide results with productIds:
+example: localhost:5000/search/Laptop Computers
---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const request = require ('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const apiKey = "af54ac8a0db812064abe2439fb08e7c5";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+//you can hide this api key outside this file and require a user of the api to provide their own key using a function to replace baseUrl
+//function would be: const returnScraperApiUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+//then you would add ( const { api_key } = req.query )to each route below, this destruct the api key from the url they type in
+//and also change the {baseUrl} to ${api_key} to pass the users key to the request
+
+
+app.use(express.json());// parses JSON
+
+
+//first route, request and response
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API.');
+});
+
+
+//GET product details... :productId means it is going to be dynamic
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));//sends response to server to display
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+//GET product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));//sends response to server to display
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+//GET product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));//sends response to server to display
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+//GET Search Results
+app.get('/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=/${searchQuery}`);
+
+        res.json(JSON.parse(response));//sends response to server to display
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
